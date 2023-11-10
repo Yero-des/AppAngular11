aab52dc06f8a41677c54064b1c65721b3b9a3d71
Yero-des <yero_zc@hotmail.com>
2023-11-09 19:32:28 -0500

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appPractica';
+  contador = 1;
+  incrementar() {
+    this.contador++;
+  }
+  decrementar() {
+    this.contador--;
+  }
 }
